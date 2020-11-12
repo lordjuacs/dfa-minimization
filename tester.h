7120249dbbfb054dc351458c37898a8ba434300d
DiegoOrtizS <52261652+DiegoOrtizS@users.noreply.github.com>
2020-11-12 15:31:37 -0500

    Update tester.h

--- tester.h
+++ tester.h
@@ -6,18 +6,21 @@
 class Tester
 {
   public:
-    Tester(int n)
+    Tester(int n, int m)
     {
       for (int i = 0; i < n; ++i)
       {
         std::cout << "\nTest n.°" << i << "\n-----------------\n";
 
-        DFA dfa(rand()%6 + 2);
-        dfa.question1();
-        dfa.question2();
-        //assert(dfa.getSize() >= 2);
-        //std::cout << "Assert test n.° " << i << "\n";
-        //std::cout << "Size: " << dfa.getSize() << "\n";
+        DFA dfa(rand()%6 + m);
+        //dfa.question1();
+        //dfa.question2();
+        auto results1 = dfa.question1();
+        auto results2 = dfa.question2();
+        // Verifico que el nuevo size de estados totales sea el mismo
+        assert(results1.first == results2.first);
+        // Verifico que el tiempo de ejecución de la pregunta 2 sea menor al del 1
+        assert(results1.second > results2.second);
       }
     }
 };
