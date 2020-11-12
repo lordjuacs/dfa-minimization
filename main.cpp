d5f86a686c2a8ac40763b24dce79c145fe0ecc52
DiegoOrtizS <52261652+DiegoOrtizS@users.noreply.github.com>
2020-11-12 16:51:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,15 @@
 
 int main() {
     srand(time(NULL));
-    Tester tester(10, 2);
-    //DFA dfa;
-    //std::cout << dfa.question1() << "\n";
-    //std::cout << dfa.question2() << "\n";
-    //assert(dfa.question1() == dfa.question2());
-    //dfa.question1();
-    //dfa.question2();
-    //std::cout << dfa.getSize() << std::endl;
-    //question1();
-    //question2();
+    Tester tester(100, 2);
+    /*
+    DFA dfa;
+    auto results1 = dfa.question1();
+    auto results2 = dfa.question2();
+    // Verifico que el nuevo size de estados totales sea el mismo
+    assert(results1.first == results2.first);
+    // Verifico que el tiempo de ejecución de la pregunta 2 sea menor al del 1
+    assert(results1.second >= results2.second);*/
 
 
     return 0;
