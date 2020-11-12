1ae967d65985baf13b1755af9b7aef7f53ddbb62
DiegoOrtizS <52261652+DiegoOrtizS@users.noreply.github.com>
2020-11-12 17:08:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-//#include "brzozowski.h"
-//#include "equivalent.h"
-//#include "dfa.h"
 #include "tester.h"
 
 int main() {
     srand(time(NULL));
-    Tester tester(10000, 2);
+    Tester tester(100, 2);
     
     /*
     DFA dfa;
@@ -14,28 +11,28 @@ int main() {
     // Verifico que el nuevo size de estados totales sea el mismo
     assert(results1.first == results2.first);
     // Verifico que el tiempo de ejecución de la pregunta 2 sea menor al del 1
-    assert(results1.second >= results2.second);*/
+    //assert(results1.second >= results2.second);*/
 
-    /*
-      Figura 4.8 Un autómata con estados equivalentes.	// Figura 4.8 Un autómata con estados equivalentes.
-      8 0 1 2	8 0 1 2
-      0 0 1	0 0 1
-      0 1 5	0 1 5
-      1 0 6	1 0 6
-      1 1 2	1 1 2
-      2 0 0	2 0 0
-      2 1 2	2 1 2
-      3 0 2	3 0 2
-      3 1 6	3 1 6
-      4 0 7	4 0 7
-      4 1 5	4 1 5
-      5 0 2	5 0 2
-      5 1 6	5 1 6
-      6 0 6	6 0 6
-      6 1 4	6 1 4
-      7 0 6	7 0 6
-      7 1 2	7 1 2
-     */
+/*
+Figura 4.8 Un autómata con estados equivalentes.	// Figura 4.8 Un autómata con estados equivalentes.
+8 0 1 2
+0 0 1
+0 1 5
+1 0 6
+1 1 2
+2 0 0
+2 1 2
+3 0 2
+3 1 6
+4 0 7
+4 1 5
+5 0 2
+5 1 6
+6 0 6
+6 1 4
+7 0 6
+7 1 2
+*/
 
     return 0;
 }
