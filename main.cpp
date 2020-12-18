56c24b818f42b9ff3301d4f94ea166c2bd5dac22
lordjuacs <juacsr@gmail.com>
2020-12-17 21:47:35 -0500

    finished

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include "tester.h"
 
 int main() {
-    //Para correr el tester
+    //Run the tester
+
     /*
     srand(time(NULL));
     Tester tester(1000, 2, false);
     */
 
-    //Para correr con inputs
-    /*
-    DFA dfa;
+    //Run with inputs
+
+    /*DFA dfa;
     auto results1 = dfa.question1();
-    auto result2 = dfa.question2();
-    auto result3 = dfa.question3();
-    auto result4 = dfa.question4();
-    auto result5 = dfa.question5();
+    auto results2 = dfa.question2();
+    auto results3 = dfa.question3();
+    auto results4 = dfa.question4();
+    auto results5 = dfa.question5();
     assert(results1.first == results5.first);
-    assert(results1.first == results4.first);
-    */
+    assert(results1.first == results4.first);*/
+
 
 
     //Coded by:
